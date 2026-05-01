47543fbdc5929c31463b8abfe85348f5a57cc261
Abdul Samhoon <bdlsmhn@gmail.com>
2026-05-01 21:05:47 +0530

    Add files via upload

---
+++ sql-queries/Create-Tables.sql
@@ -0,0 +1,38 @@
+-- Create Table company 
+CREATE TABLE companies(
+    company_id FLOAT,
+    company_name VARCHAR(255)
+);
+
+-- Create table jobs
+CREATE TABLE jobs (
+    job_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    company_id FLOAT,
+    location VARCHAR(255),
+    experience_level VARCHAR(100),
+    work_type VARCHAR(50),
+    formatted_work_type VARCHAR(50),
+    application_type VARCHAR(100),
+    sponsored INT
+);
+
+-- Create Job Metrics Table
+CREATE TABLE job_metrics (
+    job_id INT,
+    views FLOAT,
+    applies FLOAT
+);
+
+-- Create Salaries Table
+CREATE TABLE salaries(
+    min_salary FLOAT,
+    max_salary FLOAT
+);
+
+-- Create Time Table
+CREATE TABLE job_time (
+    job_id INT,
+    listed_time DATETIME,
+    original_listed_time DATETIME
+);
